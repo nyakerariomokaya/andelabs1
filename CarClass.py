66ebb2ffddddb3a29c8df5462a1600d60847b590
nyakerariomokaya <mokayanyakerario@gmail.com>
2017-02-08 10:48:17 +0300

    second commmit

--- CarClass.py
+++ CarClass.py
@@ -1,33 +1,33 @@
 class Car(object):
-      def __init__(self,name="General",model="GM",car_type="saloon",num_of_doors=4,num_of_wheels=4,speed=0):
-          self.name=name
-          self.model=model
-          self.car_type = car_type
-          self.no_of_doors=no_of_doors
-          self.no_of_wheels=no_of_wheels
-          self.speed=speed
+    def __init__(self, name='General', model='GM', car_type='saloon', num_of_doors=4, num_of_wheels=4, speed=0):
+        self.name = name
+        self.model = model
+        self.car_type = car_type
+        self.num_of_doors = num_of_doors
+        self.num_of_wheels = num_of_wheels
+        self.speed = speed
+       
 
-          if self.name =='Porshe' or 'Koenigsegg':
-             self.num_of_doors = 2
-          else:
-             self.num_of_doors=4
-     
-          if self.car_type =='trailer':
-             self.num_of_wheels=8
-          else:
-             self.num_of_wheels=4           
+        if self.name =='Porshe' or self.name == 'Koenigsegg':
+           self.num_of_doors = 2
+        else:
+           self.num_of_doors = 4
+    
+        if self.car_type =='trailer':
+           self.num_of_wheels=8
+                   
                 
-      def is_saloon(self):
+    def is_saloon(self):
           if self.car_type !='trailer':
-             self.car_type =='saloon'
              return True   
+          return False
          
-      def drive(self, moving_speed):
-          if moving_speed ==1000:
-             self.speed=3
-          elif moving_speed==77:
-              self.speed=7
-              return self
+    def drive(self, moving_speed):
+          if moving_speed ==3:
+             self.speed=1000
+          elif moving_speed==7:
+              self.speed=77
+          return self
 
                   
 
